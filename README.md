efbe8bef9fd8b12b17ee35ce17527ee2af87bb57
Ryan Routsong <rxroutsong@gmail.com>
2010-02-16 20:28:58 -0800

    Continued README and polished up the rest of the methods

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 #OneBusAway Wrapper
 
 So far only supports look up by stop id, will finish the remainder.
+
+====================
+
+#Calls
+
+## *stop_by_id(STOP_ID)*
+> require 'lib/onebusaway'
+> onebusaway = OneBusAway.new 'YOUR_KEY'
+> onebusaway.stop_by_id('1_10020')
+> **{"text"=>"OK", "data"=>{"stop"=>{"id"=>"1_100...**
+
+## *route_by_id(ROUTE_ID)*
+> require 'lib/onebusaway'
+> onebusaway = OneBusAway.new 'YOUR_KEY'
+> onebusaway.route_by_id('1_30')
+> **{"text"=>"OK", "data"=>{"route"=>{"id"=>"1_100...**
+
+*More to come.*
--- lib/onebusaway.rb
+++ lib/onebusaway.rb
@@ -2,31 +2,45 @@ require 'net/http'
 require 'json'
 
 class OneBusAway
-	def initialize
-		@@key = 'YOUR_KEY'
-		@@api = 'http://api.onebusaway.org/api/where'
+	def initialize key
+		@key = key
+		@api = 'http://api.onebusaway.org/api/where'
 	end
 
-	def self.stopbyid id
-		url		= "#{@@api}/stop/#{id}.json?key=#{@@key}"
+	def stop_by_id id
+		url		= "#{@api}/stop/#{id}.json?key=#{@key}"
 		data	= Net::HTTP.get_response(URI.parse(url)).body
-
 		JSON.parse(data)
 	end
 
-	def RouteById id
+	def route_by_id id
+		url		= "#{@api}/route/#{id}.json?key=#{@key}"
+		data	= Net::HTTP.get_response(URI.parse(url)).body
+		JSON.parse(data)
 	end
 
-	def StopByAddress address
+	def stopsbylatlong lat,lon
+		url		= "#{@api}/stops-for-location.json?key#{@key}&lat=#{lat}&lon=#{lon}"
+		data	= Net::HTTP.get_response(URI.parse(url)).body
+		JSON.parse(data)
 	end
 
-	def StopsForRoute route
+	def routesbylatlon lat,lon
+		url		= "#{@api}/routes-for-location.json?key=#{@key}&lat=#{lat}&lon=#{lon}"
+		data	= Net::HTTP.get_response(URI.parse(url)).body
+		JSON.parse(data)
 	end
 
-	def ArrivalsForStop stop
+	def stopsforroute route
+		url		= "#{@api}/stops-for-route/#{route}.json?key=#{@key}"
+		data	= Net::HTTP.get_response(URI.parse(url)).body
+		JSON.parse(data)
 	end
 
-	def DepartsForStop stop
+	def arvdepforstop route
+		url		= "#{@api}/arrivals-and-departures-for-stop/#{route}.json?key=#{@@key}"
+		data	= Net::HTTP.get_response(URI.parse(url)).body
+		JSON.parse(data)
 	end
 end
 
